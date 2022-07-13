af57bfcbb80262919bbf63b50c4efbd15d0c98bd
Max Netterberg <max.netterberg@gmail.com>
2022-07-13 22:23:27 +0200

    Added more tags

--- services/config/scripts/build.mjs
+++ services/config/scripts/build.mjs
@@ -4,6 +4,6 @@ import { $ } from "zx";
 
 const { stdout: sha } = await $`git rev-parse --short HEAD`;
 
-await $`docker build . -t maxrsb/rsb-config:${sha.trim()},maxrsb/rsb-config:latest`;
+await $`docker build . -t maxrsb/rsb-config:${sha.trim()} -t maxrsb/rsb-config:latest`;
 
 await $`docker push`;
