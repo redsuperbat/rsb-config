b8912119a37ae56be6bfab3babad2102824d1f19
Max Netterberg <max.netterberg@gmail.com>
2022-07-13 22:21:47 +0200

    testing build script

--- services/config/scripts/build.mjs
+++ services/config/scripts/build.mjs
@@ -4,4 +4,6 @@ import { $ } from "zx";
 
 const { stdout: sha } = await $`git rev-parse --short HEAD`;
 
-await $`docker build . -t rsb/config:${sha.trim()}`;
+await $`docker build . -t maxrsb/rsb-config:${sha.trim()},maxrsb/rsb-config:latest`;
+
+await $`docker push`;
